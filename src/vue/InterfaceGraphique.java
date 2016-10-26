840b7a4d75a0c5cb339e81baf38e9aea7d1c2c98
Mathieu Porcel <mathieu.porcel@gmail.com>
2016-10-26 19:15:46 +0200

    Base vues

---
+++ src/vue/InterfaceGraphique.java
@@ -0,0 +1,33 @@
+package vue;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class InterfaceGraphique extends JFrame {
+
+    private static InterfaceGraphique instance;
+
+    public static InterfaceGraphique getInstance() {
+        if (instance == null) {
+            instance = new InterfaceGraphique();
+        }
+        return instance;
+    }
+
+    private InterfaceGraphique() {
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void setVueActions(VueActions vue) {
+        add(vue, BorderLayout.WEST);
+    }
+
+    public void setVuePrincipale(VuePrincipale vue) {
+        add(vue, BorderLayout.CENTER);
+    }
+}
