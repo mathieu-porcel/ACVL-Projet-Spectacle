9665b391fd2024d47df016c85b382bf4bb09000f
Mathieu Porcel <mathieu.porcel@gmail.com>
2016-10-26 19:08:04 +0200

    Singletons

--- src/Controleur.java
+++ src/Controleur.java
@@ -1,4 +1,16 @@
 
 public class Controleur {
 
+    private static Controleur instance;
+
+    public static Controleur getInstance() {
+        if (instance == null) {
+            instance = new Controleur();
+        }
+        return instance;
+    }
+
+    private Controleur() {
+
+    }
 }
--- src/vue/Interface.java
+++ src/vue/Interface.java
@@ -2,4 +2,16 @@ package vue;
 
 public class Interface {
 
+    private static Interface instance;
+
+    public static Interface getInstance() {
+        if (instance == null) {
+            instance = new Interface();
+        }
+        return instance;
+    }
+
+    private Interface() {
+
+    }
 }
