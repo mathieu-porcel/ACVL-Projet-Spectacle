4c06a7b7863c35a0eb48d1a04dc763b3b3fad7aa
Mathieu Porcel <mathieu.porcel@gmail.com>
2016-10-28 00:05:32 +0200

    Fix zone

--- src/modele/Zone.java
+++ src/modele/Zone.java
@@ -6,20 +6,21 @@ import java.util.ArrayList;
 public class Zone {
 
     public int numero;
-    
+
     public Categorie categorie;
     public ArrayList<ArrayList<Place>> places;
-    
+
     public Zone(int numero, Categorie categorie) {
         this.numero = numero;
         this.categorie = categorie;
+        this.places = new ArrayList<>();
     }
-    
-    public void addRang(){
-        places.add(new ArrayList());
+
+    public void addRang() {
+        places.add(new ArrayList<Place>());
     }
-    
-    public void addNumero(int rang){
+
+    public void addNumero(int rang) {
         places.get(rang).add(new Place(rang, numero));
     }
 }
