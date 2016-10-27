cafd3715e1a72db237d92f6b641d99982da47370
Mathieu Porcel <mathieu.porcel@gmail.com>
2016-10-28 01:06:47 +0200

    vue gestions comptes

--- src/vue/VueActionsAdmin.java
+++ src/vue/VueActionsAdmin.java
@@ -2,13 +2,23 @@ package vue;
 
 import java.util.ArrayList;
 
+import Controleur.Controleur;
+import modele.Compte;
+import modele.TypeComte;
+
 @SuppressWarnings("serial")
 public class VueActionsAdmin extends AbstractVueActions {
     public VueActionsAdmin() {
-        addAction("Gestions des comptes", () -> {});
+        // TODO: use controller
+        addAction("Gestions des comptes", () -> {
+            // TODO: get users from model
+            ArrayList<Compte> comptes = new ArrayList<>();
+            comptes.add(new Compte("admin", "admin", TypeComte.Admin, "", "admin", "admin"));
+            InterfaceGraphique.getInstance().setVuePrincipale(new VueGestionComptes(comptes));
+        });
         addAction("Modifier la salle", () -> InterfaceGraphique.getInstance().setVuePrincipale(new VueEditSalle(new ArrayList<>()) {}));
         addAction("Modifier les tarifs", () -> InterfaceGraphique.getInstance().setVuePrincipale(new VueEditTarifs(new ArrayList<>()) {}));
         addAction("Archiver la base de donnee", () -> {});
-        addAction("Deconnexion", () -> Controleur.Controleur.getInstance().deconnection());
+        addAction("Deconnexion", () -> Controleur.getInstance().deconnection());
     }
 }
---
+++ src/vue/VueGestionComptes.java
@@ -0,0 +1,27 @@
+package vue;
+
+import java.util.List;
+
+import modele.Compte;
+
+@SuppressWarnings("serial")
+public class VueGestionComptes extends AbstractVuePrincipale {
+    public VueGestionComptes(List<Compte> comptes) {
+        addTitre("Ajouter un nouveau compte:");
+        addBouton("Creer", () -> InterfaceGraphique.getInstance().setVuePrincipale(new VueInscription()));
+        newLigne();
+
+        addTitre("Liste des comptes:");
+        newLigne();
+        for (Compte compte : comptes) {
+            addTexte("");
+            addTexte(compte.nom);
+            addTexte(compte.prenom);
+            addTexte("(" + compte.login + ")");
+            addBouton("Usurper", () -> {
+                // TODO
+            });
+            newLigne();
+        }
+    }
+}
