7384db961ab1b613b170163bac94bb6cdd6b6c60
Mathieu Porcel <mathieu.porcel@gmail.com>
2016-10-27 14:13:33 +0200

    Organisation Vue Principale

--- src/vue/VueConnexion.java
+++ src/vue/VueConnexion.java
@@ -1,27 +1,21 @@
 package vue;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import java.util.function.Supplier;
 
 @SuppressWarnings("serial")
 public class VueConnexion extends VuePrincipale {
     public VueConnexion() {
-        addComponent(new JLabel("Pseudo"));
-        JTextField pseudo = new JTextField(10);
-        addComponent(pseudo);
-        newLine();
+        addTexte("Login");
+        Supplier<String> login = addChampTexte();
+        newLigne();
 
-        JPasswordField motDePasse = new JPasswordField(10);
-        addComponent(new JLabel("Mot de passe"));
-        addComponent(motDePasse);
-        newLine();
+        addTexte("Mot de passe");
+        Supplier<String> motDePasse = addChampMotDePasse();
+        newLigne();
 
-        JButton ok = new JButton("Ok");
-        ok.addActionListener(e -> {
-            System.out.println("Connexion: " + pseudo.getText() + " " + new String(motDePasse.getPassword()));
+        addTexte("");
+        addBouton("Connexion", () -> {
+            System.out.println("Connexion: " + login.get() + " " + motDePasse.get());
         });
-        addComponent(ok);
     }
 }
--- src/vue/VuePrincipale.java
+++ src/vue/VuePrincipale.java
@@ -1,32 +1,77 @@
 package vue;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.util.function.Supplier;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public abstract class VuePrincipale extends JPanel {
 
-    private JPanel currentLine;
+    private JPanel panel;
+    private GridBagConstraints constraints;
 
     public VuePrincipale() {
-        setLayout(new GridBagLayout());
-        newLine();
+        // ScrollPane acceuillant les composnants graphiques
+        setLayout(new BorderLayout());
+        panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        constraints = new GridBagConstraints();
+        add(new JScrollPane(panel), BorderLayout.CENTER);
+
+        // Contraintes du cadrillage des composnants graphiques
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.insets = new Insets(2, 2, 2, 2);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        constraints.fill = GridBagConstraints.BOTH;
     }
 
-    protected void newLine() {
-        currentLine = new JPanel();
-        currentLine.setLayout(new FlowLayout(FlowLayout.CENTER));
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 1;
-        add(currentLine, constraints);
+    protected JComponent addComposantGraphique(JComponent component) {
+        constraints.gridx++;
+        panel.add(component, constraints);
+        panel.updateUI();
+        return component;
     }
 
-    protected void addComponent(JComponent component) {
-        currentLine.add(component);
-        currentLine.updateUI();
+    protected void newLigne() {
+        constraints.gridx = 0;
+        constraints.gridy++;
+    }
+
+    protected void addBouton(String texte, Runnable action) {
+        JButton bouton = new JButton(texte);
+        bouton.addActionListener(e -> {
+            action.run();
+        });
+        addComposantGraphique(bouton);
+    }
+
+    protected void addTexte(String texte) {
+        addComposantGraphique(new JLabel(texte));
+    }
+
+    protected Supplier<String> addChampTexte() {
+        JTextField champ = new JTextField(16);
+        addComposantGraphique(champ);
+        return () -> champ.getText();
+    }
+
+    protected Supplier<String> addChampMotDePasse() {
+        JPasswordField champ = new JPasswordField(16);
+        addComposantGraphique(champ);
+        return () -> new String(champ.getPassword());
     }
 }
